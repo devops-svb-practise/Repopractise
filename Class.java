297411ad7363663adf6619ec607f42e15f126866
kirandevops6229 <dmkiran6229@gmail.com>
2021-02-03 22:41:34 +0530

    branches concnept

--- Class.java
+++ Class.java
@@ -1,4 +1,5 @@
 Hello Team
-Welcome to Java -- Updating the code to check the branch concept
+in dev branch
+welcome to Java -- Updating the code to check the branch concept in dev branch
 Git commands
 end
