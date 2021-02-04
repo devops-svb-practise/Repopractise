23a97e34d8063acf70c42ded464e7b4a2e4237e9
kirandevops6229 <dmkiran6229@gmail.com>
2021-02-04 17:37:25 +0530

    updated class

--- Class.java
+++ Class.java
@@ -1,5 +1,5 @@
-Hello Team
+Hello Team --  adding changes to tag
 in dev branch
-welcome to Java -- Updating the code to check the branch concept in dev branch
+welcome to Java -- Updating the code to check the branch concept in dev branch 
 Git commands
 end
